53163c5d198ac0f9a1c5d29dec64e27dcbd040f1
Guillaume Aubert <guillaume.aubert@gmail.com>
2013-03-28 17:26:14 +0100

    add stuff

---
+++ app/initialization.js
@@ -0,0 +1,59 @@
+/**
+ * Application initialization
+ */
+
+'use strict';
+
+var fs             = require('fs'),
+	/* appConfig      = require('./appConfig.js'), */
+	/* storage        = require('./storage.js'), */
+	/* jadeManager    = require('./jadeManager.js'), */
+	/* fileWatcher    = require('./fileWatcher.js'), */
+	/* projectManager = require('./projectManager.js'), */
+	/* notifier       = require('./notifier.js'), */
+	/* util           = require('./util.js'); */
+
+/* var	historyDb      = storage.getHistoryDb(), */
+	$              = global.jQuery;
+
+//share global context
+/* global.appConfig = appConfig; */
+
+//just for debug
+/*if (appConfig.getAppPackage().appinfo.debug) {
+	global.storage = storage;
+	global.fileWatcher = fileWatcher;
+	global.notifier = notifier;
+	global.projectManager = projectManager;
+}
+*/
+
+/**
+ * render main window view
+ */
+function renderMainWindow () {
+	var targetMainPage = process.cwd() + '/html/main.html';
+
+	var html = fs.readFileSync(targetMainPage, 'utf8');
+
+	$('#window').append(html);
+}
+
+exports.init = function() {
+	//Add error event listener
+	process.on('uncaughtException', function(e) {
+		mainWindow.show();
+		notifier.throwAppError(e.stack);
+	});
+
+    $( "input[type=submit], a, button" )
+      .button()
+      .click(function( event ) {
+        event.preventDefault();
+      });
+	
+	//rander main window view
+	renderMainWindow();
+
+	mainWindow.show();
+}
