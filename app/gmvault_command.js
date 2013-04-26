b10723e37b6e19da718192fe295cdc119879eecb
Guillaume Aubert <guillaume.aubert@gmail.com>
2013-04-26 18:09:16 +0200

    work on handling inputs

--- app/gmvault_command.js
+++ app/gmvault_command.js
@@ -6,7 +6,8 @@
 
  var fs             = require('fs');
  
- var gmvaultPath    = '/Users/gaubert/gmvault-v1.8.1/bin/gmvault';
+ //var gmvaultPath    = '/Users/gaubert/gmvault-v1.8.1/bin/gmvault';
+ var gmvaultPath    = '/home/aubert/test_echo.sh';
  
  
  var $              = global.jQuery;
@@ -30,22 +31,25 @@ String.prototype.beginsWith = function (string) {
      
      var params = [verb].concat(args);
  
-     var gmv   = spawn('gmvault', params);
+     //var gmv   = spawn('gmvault', params);
+     var gmv   = spawn(gmvaultPath, params);
 
-        gmv.stdout.on('data', function (data) {
-            global.debug('stdout: ' + data.constructor.name);
-            var val = $("#mTextArea").val();
-            $("#mTextArea").val(val + data);
-            if (data.indexOf('[gmv-msg]:') === 0) {
-                
-               $("#mTextArea").val('[MSG in JS]:' + data.substring(10) + val); 
-                
-            }
-            
+     gmv.stdout.on('data', function (data) {
+         global.debug('========= stdout: ' + data);
+		 var out = data.toString();
+         var val = $("#mTextArea").val();
+		 var lines = out.split("\n")
+		 var res = "";
+		 for (var i=0; i < lines.length; i++) {
+           if (lines[i].indexOf('[gmv-msg]:') === 0) {
+		       res = "[MSG-JS]:" + lines[i].substring(10) + "\n" + res;
+           }
+		 }
+         $("#mTextArea").val(res + val); 
 			
-        });
+     });
 
-        gmv.stderr.on('data', function (data) {
+     gmv.stderr.on('data', function (data) {
             global.debug('stderr: ' + data);
             var val = $("#mTextArea").val();
 			$("#mTextArea").val('[stderr]:' + data + val);
@@ -53,15 +57,15 @@ String.prototype.beginsWith = function (string) {
 			//cpt = cpt + 1;
         });
 
-        gmv.on('close', function (code) {
+     gmv.on('close', function (code) {
             global.debug('gmvault_cmd.run[close event]: Exited with code ' + code);
         });
         
-        gmv.on('error', function(err) {
+     gmv.on('error', function(err) {
             global.debug('gmvault_cmd.run[error event]: Received error message' + err);
         });
         
-        gmv.on('exit', function(code, signal) {
+     gmv.on('exit', function(code, signal) {
             global.debug('gmvault_cmd.run[exit event]:' + code + " signal=" + signal);
         });
         
